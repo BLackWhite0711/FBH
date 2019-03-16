f91ac21b29b3c2a09937d308fbbe905eac9b7eae
Muhammadfathul <46747652+muhammadfathul@users.noreply.github.com>
2019-03-17 02:57:46 +0700

    Update .upgrade.sh

--- .upgrade.sh
+++ .upgrade.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #upgrade.sh
 clear
+#color
+blue='\e[1;34m'
+green='\e[0;23m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
 echo -e $purple "Upgrade... Tool FBH"
 sleep 3
 cd $HOME
