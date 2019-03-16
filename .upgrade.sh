8ff2ce82c3cfdb2a72bce42e813269cf4a3ab7ac
Muhammadfathul <46747652+muhammadfathul@users.noreply.github.com>
2019-03-17 03:34:44 +0700

    Add files via upload

---
+++ .upgrade.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#upgrade.sh
+clear
+#color
+blue='\e[1;34m'
+green='\e[0;23m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+echo -e $purple "Upgrade... Tool $Cyan FBH"
+sleep 2
+echo -e $red "Loading..."
+sleep 2
+rm -rf .FBH .call.php .wa.php .create.py .FBHtarget.py .FBHrombongan.py .hammer.py .wifite.py setup
+sleep 1
+curl -o .FBH https://raw.githubusercontent.com/muhammadfathul/FBH/master/.FBH
+curl -o .FBHtarget.py https://raw.githubusercontent.com/muhammadfathul/FBH/master/.FBHtarget.py
+curl -o .FBHrombongan.py https://raw.githubusercontent.com/muhammadfathul/FBH/master/.FBHrombongan.py
+curl -o .call.php https://raw.githubusercontent.com/muhammadfathul/FBH/master/.call.php
+curl -o .wifite.py https://raw.githubusercontent.com/muhammadfathul/FBH/master/.wifite.py
+curl -o .wa.php https://raw.githubusercontent.com/muhammadfathul/FBH/master/.wa.php
+curl -o .hammer.py https://raw.githubusercontent.com/muhammadfathul/FBH/master/.hammer.py
+curl -o setup https://raw.githubusercontent.com/muhammadfathul/FBH/master/setup
+curl -o .create.py https://raw.githubusercontent.com/muhammadfathul/FBH/master/.create.py
+sleep 2
+chmod +x *
+bash .FBH
